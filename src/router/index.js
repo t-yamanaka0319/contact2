bde8b2a9148866e9006464afe018e9f3c39899d7
yamanaka <winc215yamanaka@gmail.com>
2021-01-05 15:00:29 +0900

    built application

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import('../views/Contact.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
